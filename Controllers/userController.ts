6168eb42b2424ecda6822cb5a67220bd1fa1dcd2
MRPERFECT0603 <vivekshaurya62@gmail.com>
2025-01-08 15:45:24 +0530

    corrected all the errors in the code as mentioned in the PR

--- Controllers/userController.ts
+++ Controllers/userController.ts
@@ -1,65 +1,88 @@
-import { Request, Response  } from "express";
-import  pool  from "../config/db";
+import { Request, Response } from "express";
+import pool from "../config/db";
+import { User } from "../Interfaces/User";
+import validator from 'validator';
 
 
-interface User {
-    id: number;
-    username?: string;
-    email?: string;
-    bio?: string;  
-}
+const addUser = async (req: Request, res: Response) => {
+    const { username, email } = req.body;
 
-const addUser = async (req : Request, res : Response)=>{
-   const { username  , email  } = req.body;
-   try{
+    if (!validator.isEmail(email))
+        return res.status(400).json({ error: 'Invalid email format' });
+    // Username should be length(6-20) && should be alphanumeric (Containing only alphabets and digits).
+    if (!validator.isLength(username, { min: 6, max: 20 }) || !validator.isAlphanumeric(username))
+        return res.status(400).json({ error: 'Invalid Username format' });
+
+
+    try {
         const userQuery = "INSERT INTO \"Users\" (username , email) VALUES ($1,$2) RETURNING *";
-        const result = await pool.query<User>(userQuery , [username, email]);
+        const result = await pool.query<User>(userQuery, [username, email]);
         const createdUser = result.rows[0];
         return res.status(201).json(createdUser);
-   }
-   catch(error){
-        console.error("Error in creating user:", error);
-        return res.status(500).send("Error in creating the user.");
-   }
-}
-
-const getUser = async (req : Request, res : Response)=>{
-    const {userId}  = req.params;
-    try{
-         const userQuery = "SELECT * FROM \"Users\" WHERE id = $1;";
-         const result = await pool.query<User>(userQuery , [userId]);
-         const createdUser = result.rows[0];
-         return res.status(200).json(createdUser);
     }
-    catch(error){
-         console.error("Error in getting user:", error);
-         return res.status(500).send("Error in getting the user.");
+    catch (error) {
+        const err = error as Error; 
+        console.error("Error in creating user:", {
+            error: err.message || err,
+            requestBody: req.body,
+            requestHeaders: req.headers,
+            requestParams: req.params
+        });
+    
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
-const updateUser = async (req : Request, res : Response)=>{
-    const {userId}  = req.params;
+const getUser = async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    if (!validator.isUUID(userId))
+        return res.status(400).json({ error: 'Invalid UserId format' });
+
+    try {
+        const userQuery = "SELECT * FROM \"Users\" WHERE id = $1;";
+        const result = await pool.query<User>(userQuery, [userId]);
+        return res.status(200).json(result.rows[0]);
+    }
+    catch (error) {
+        const err = error as Error; 
+        console.error("Error in creating user:", {
+            error: err.message || err,
+            requestBody: req.body,
+            requestHeaders: req.headers,
+            requestParams: req.params
+        });
+    
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+const updateUser = async (req: Request, res: Response) => {
+    const { userId } = req.params;
     const { username, email, bio } = req.body;
+    
+    if (!validator.isUUID(userId))
+        return res.status(400).json({ error: 'Invalid UserId format' });
 
-    let setClause = []; 
-    let queryParams = [];  
+
+    let setClause = [];
+    let queryParams = [];
 
     if (username) {
         setClause.push(`username = $${setClause.length + 1}`);
-        queryParams.push(username);  
+        queryParams.push(username);
     }
     if (email) {
         setClause.push(`email = $${setClause.length + 1}`);
-        queryParams.push(email); 
+        queryParams.push(email);
     }
     if (bio) {
         setClause.push(`bio = $${setClause.length + 1}`);
-        queryParams.push(bio);  
+        queryParams.push(bio);
     }
     if (setClause.length === 0) {
         return res.status(400).json({ message: "No data to update." });
     }
-    queryParams.push(userId); 
+    queryParams.push(userId);
     const userQuery = `
         UPDATE "Users" 
         SET ${setClause.join(', ')} 
@@ -67,15 +90,23 @@ const updateUser = async (req : Request, res : Response)=>{
         RETURNING *;
     `;
 
-    try{
-         const result = await pool.query<User>(userQuery , queryParams);
-         const createdUser = result.rows[0];
-         return res.status(200).json(createdUser);
+    try {
+        const result = await pool.query<User>(userQuery, queryParams);
+        const updtaedUser = result.rows[0];
+        return res.status(200).json(updtaedUser);
     }
-    catch(error){
-         console.error("Error in updating user:", error);
-         return res.status(500).send("Error in updating the user.");
-    }
+    catch (error) {
+        const err = error as Error; 
+        console.error("Error in creating user:", {
+            error: err.message || err,
+            requestBody: req.body,
+            requestHeaders: req.headers,
+            requestParams: req.params
+        });
+    
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }       
+
 }
 
 module.exports = {
